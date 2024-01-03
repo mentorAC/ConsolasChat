e97adb16963b7f3c94567369bbb1179edd50a0d9
maxpe3447 <mpechersky16@gmail.com>
2024-01-03 17:26:47 +0200

    add Server.cs, ClientObject.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,1 @@
-﻿Console.WriteLine("hello world");
-int a = 10;
-int b = 100;
-string str;
+﻿
---
+++ Server/Server.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Server
+    {
+        public List<ClientObject> Clients { get; } = new List<ClientObject>();
+    }
+}
