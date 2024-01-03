1dd9191ad82e9323502c9c9e0a782aad4ac562b8
maxpe3447 <mpechersky16@gmail.com>
2024-01-03 17:55:46 +0200

    update ClientObject

--- Server/ClientObject.cs
+++ Server/ClientObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,26 @@ namespace Server
 {
     internal class ClientObject
     {
+        private readonly TcpClient client;
+        private readonly Server server;
 
+        private string Id { get; } = Guid.NewGuid().ToString();
+        private StreamReader Reader { get; }
+        public ClientObject(TcpClient client, Server server)
+        {
+            this.client = client;
+            this.server = server;
+
+            var stream = client.GetStream();
+
+            Reader = new StreamReader(stream);
+
+        }
+
+
+        public void ProcessAsync()
+        {
+
+        }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,1 +1,2 @@
-﻿
+﻿Console.WriteLine(Guid.NewGuid().ToString());
+Console.WriteLine();
